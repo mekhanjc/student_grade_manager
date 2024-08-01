6e03905e4748d49603d95d7a69746c9f9e4043d0
dumdumy <duddlrjek1@gmail.com>
2024-08-01 12:00:52 +0900

    add student.h

---
+++ manager/student.h
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+using namespace std;
+
+class Student {
+	string name;
+	string subject;
+	int id;
+	double avgScore = 0;
+public:
+	Student(string name, string subject, int id);
+	string getName();
+	void setName(string name);
+	string getSubject();
+	void setSubject(string subject);
+	void setId(int id);
+	void setavgScore(double avgScore);
+	double getavgScore();
+	virtual void setScore(int, int);
+};
+
+class StudentSci : public Student {
+	int science;
+	int math;
+public:
+	void setScore(int science, int math);
+	int getScience();
+	int getMath();
+
+};
+
+class StudentArt : public Student {
+	int economics;
+	int arts;
+public:
+	void setScore(int economics, int arts);
+	int getEconomics();
+	int getArts();
+
+};
